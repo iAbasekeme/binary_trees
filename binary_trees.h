1ed0426a5f3f5951131d372da3f2be7bcd3ba664
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-02 15:35:49 +0100

    Update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -1,5 +1,7 @@
-#ifndef BINARY_TREES_H
-#define BINARY_TREES_H
+#ifndef _BINARY_TREES_H
+#define _BINARY_TREES_H
+#include <stdio.h>
+#include <stddef.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -15,12 +17,12 @@ struct binary_tree_s
     struct binary_tree_s *parent;
     struct binary_tree_s *left;
     struct binary_tree_s *right;
-};
-typedef struct binary_tree_s binary_tree_t;
+} binary_tree_t;
+
+void binary_tree_print(const binary_tree_t *);
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
-
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
