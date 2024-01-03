f0ea0e429e6cd127a2352a4e7620490eb3a0af89
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-03 11:27:33 +0100

    Update 1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,6 +10,7 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newNode;
+
 	if (!parent)
 		return (NULL);
 	newNode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
