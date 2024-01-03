0311cf06111ca5a0e0ef271b0dd557a25472681f
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-03 12:00:33 +0100

    Create 3-binary_tree_delete.c

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_delete - A function that frees an entire binary tree
+ * @tree: A pointer to the root of the tree to delete
+ *
+ * Return: No return
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+    free(tree);
+}
