2ab824405f530d05174ba5faf258c4fa0ba84a39
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-03 13:22:41 +0100

    Update 3-binary_tree_delete.c

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -9,11 +9,14 @@ void binary_tree_delete(binary_tree_t *tree)
 {
     if (tree == NULL)
         return;
-    binary_tree_delete(tree->left);
+    else
+    {
+        binary_tree_delete(tree->left);
 
-    binary_tree_delete(tree->right);
+        binary_tree_delete(tree->right);
 
-    free(tree);
+        free(tree);
 
-    tree = NULL;
+        tree = NULL;
+    }
 }
