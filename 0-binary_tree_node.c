5bdce830077c03ac690dd97db1c68dbc2df27a89
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-03 11:20:55 +0100

    fixed wrong indentation

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,8 +10,8 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newNode;
-
 	newNode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+
 	if (!newNode)
 		return (NULL);
 
