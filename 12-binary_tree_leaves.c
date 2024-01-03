18bfa1e08951809b3ac9917eca12e2d0a3855760
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-03 18:50:29 +0100

    Create 12-binary_tree_leaves.c

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_leaves - A function that counts the leaves in a tree
+ * @tree: The tree involved
+ *
+ * Return: size of all the leaves
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if (tree != NULL && tree->left == NULL && tree->right == NULL)
+		return 1 + binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
+}
