8afe1d6313644d91470b29510cca98805cbceb58
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-05 02:40:07 +0100

    Update 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,4 +1,28 @@
 #include "binary_trees.h"
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - function that measures the height of a binary tree
+ * @tree: pointer to the root
+ * Return: the height of the root
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t hleft, hright;
+
+	if (tree == NULL)
+		return (-1);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+
+	/*Find recursively the height of left and right subtree*/
+	hleft = binary_tree_height(tree->left);
+	hright = binary_tree_height(tree->right);
+
+	/* return the longuest*/
+	return (1 + (hleft > hright ? hleft : hright));
+}
+
 /**
  * binary_tree_balance - A function that calculates the balance factor of a binary tree
  * @tree: A pointer to the root node of the
@@ -16,5 +40,5 @@ int binary_tree_balance(const binary_tree_t *tree)
 	height_left = binary_tree_height(tree->left);
 	height_right = binary_tree_height(tree->right);
 
-	return height_left - height_right;
+	return (height_left - height_right);
 }
